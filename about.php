58f6b43f5703844a27adb6e264474d06a74d3859
UtkarshAhuja2003 <ahujautkarsh80@gmail.com>
2023-09-16 10:57:47 +0530

    change'

--- about.php
+++ about.php
@@ -5,7 +5,7 @@
         <div class='w-72 h-[3px] mx-auto mt-2 bg-[#041C32] rounded-xl'></div>
       </div>
       <p class='text-md md:text-xl  mt-8 pb-28 text-justify'>
-      Maharaja Surajmal Institute of Technology (MSIT) in New Delhi, India, and Wentworth Institute of Technology (WIT) in Boston, MA USA, are organizing the 2nd International Conference ICAIA-2024 on Dec 2024. The conference aims to provide a platform for researchers and practitioners from both academia as well as industry to meet and share cutting-edge development in the field of Artificial Intelligence and its Applications.
+      Maharaja Surajmal Institute of Technology (MSIT) in New Delhi, India, and Wentworth Institute of Technology (WIT) in Boston, MA USA, are organizing the 2nd International Conference ICAIA-2024 on March 2024. The conference aims to provide a platform for researchers and practitioners from both academia as well as industry to meet and share cutting-edge development in the field of Artificial Intelligence and its Applications.
       </p> 
       </div>
 </div>   
