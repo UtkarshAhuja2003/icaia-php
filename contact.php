60f12be5c57fea98eab2f1b2bf0152c6f47616db
UtkarshAhuja2003 <ahujautkarsh80@gmail.com>
2023-12-07 15:56:00 +0530

    email update

--- contact.php
+++ contact.php
@@ -47,7 +47,7 @@
               </div>
               <div class="text-center">
                 <h1 class="text-gray-400 text-[22px] font-[500] mt-6">
-                  info@icaia-msit.in
+                conference2024@msit.in
                 </h1>
               </div>
             </div>
